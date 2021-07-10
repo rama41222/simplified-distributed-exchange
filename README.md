7d63f8fa9b9d350dbb56ac3f089093dd2bef69eb
rama41222 <dinushankanrg@gmail.com>
2021-07-11 01:54:25 +0530

    feat: docs

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
 # simplified-distributed-exchange
 A simplified distributed crypto exchage
 
+## Setup
+```bash
+
+# Install required packages
+npm i
+
+# Run the grape discovery servers
+./node_modules/.bin/grape --dp 20001 --aph 30001 --bn '127.0.0.1:20002'
+./node_modules/.bin/grape --dp 20002 --aph 40001 --bn '127.0.0.1:20001'
+
+# Run the server
+node ./server/src/server.js 
+
+# Run the client
+node ./client/src/client.js 
+```
+
+**Create as much clients as you like and try to execute transactions
+**Also utiloze the workflows to execute transactions
+
+```javascript
+const buyOrders = await Promise.all[
+    (createBuyOrder(1, 221, 1, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 220, 2, peer, WalletService, OrderService, clientName),
+    createBuyOrder(2, 1, 120, peer, WalletService, OrderService, clientName),
+    createBuyOrder(2, 1, 300, peer, WalletService, OrderService, clientName),
+    createBuyOrder(2, 0.9, 223, peer, WalletService, OrderService, clientName),
+    createBuyOrder(2, 1.1, 120, peer, WalletService, OrderService, clientName))
+  ];
+
+  /** Create Sell Order */
+  const sellOrders = await Promise.all[
+    (createSellOrder(2, 1, 1000, peer, WalletService, OrderService, clientName),
+    createBuyOrder(2, 1, 20000, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 1, 120, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 1, 300, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 0.9, 223, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 1.1, 120, peer, WalletService, OrderService, clientName))
+  ];
+```
+
 ## Done
 
 [X] Establish the network
@@ -13,16 +54,20 @@ A simplified distributed crypto exchage
 
 [X] Order Creation
 
-[] Order Execution from server (Via automation task );
+[X] Order Execution from server (Via automation task );
 
-[] Broadcasting to all the clients (Syncing the books in the network)
+[X] Broadcasting to all the clients (Syncing the books in the network)
 
 ## Todo
 
-[] Unit tests (No TDD)
+[ ] Handle race conditions by possibly using a QUEUE datastructure
 
-[] Documentation
+[ ] Handle wallet update after transaction from client side
 
-[] Rewire in Typescript
+[ ] Unit tests (No TDD)
 
-[] Move Endpoints to dot env
+[X] Documentation
+
+[ ] Rewire in Typescript
+
+[ ] Move Endpoints to dot env
--- client/src/client.js
+++ client/src/client.js
@@ -2,68 +2,82 @@
 // and then establishes a P2P connection it.
 // It will then send { msg: 'hello' } to the RPC server
 
-'use strict'
+"use strict";
 
-const { PeerRPCClient }  = require('grenache-nodejs-http')
-const Link = require('grenache-nodejs-link')
-const { REQUESTS } = require('../../commons/proto/services');
-const { logger } = require('../../commons/utils/logger');
-const Order = require('../../server/src/modules/orders/order.model');
-const OrderService = require('./modules/order-book/order-book.service')();
-const WalletService = require('./modules/wallet/wallet.service')();
-const CurrencyType = require('./modules/wallet/currency-type.model');
-const { createBuyOrder, createSellOrder} = require('./modules/workflows');
+const { PeerRPCClient } = require("grenache-nodejs-http");
+const Link = require("grenache-nodejs-link");
+const { REQUESTS } = require("../../commons/proto/services");
+const { logger } = require("../../commons/utils/logger");
+const OrderService = require("./modules/order-book/order-book.service")();
+const WalletService = require("./modules/wallet/wallet.service")();
+const { createBuyOrder, createSellOrder } = require("./modules/workflows");
 const clientName = "1";
 /**
  * This fuction adds seed money into wallet
  * @returns {void}
  */
 const seeder = () => {
-  WalletService.deposit(1, 100);  
-  WalletService.deposit(2, 1000000);  
+  WalletService.deposit(1, 100);
+  WalletService.deposit(2, 1000000);
   logger(200, [...WalletService.getWallet().entries()]);
-}
+};
 
 const link = new Link({
-  grape: 'http://127.0.0.1:30001'
-})
-link.start()
-const peer = new PeerRPCClient(link, {})
-peer.init()
+  grape: "http://127.0.0.1:30001",
+});
+link.start();
+const peer = new PeerRPCClient(link, {});
+peer.init();
 seeder();
 
-peer.request(REQUESTS.HANDSHAKE, { msg: 'init' }, { timeout: 10000 }, (err, data) => {
+peer.request(
+  REQUESTS.HANDSHAKE,
+  { msg: "init" },
+  { timeout: 10000 },
+  (err, data) => {
+    if (err) {
+      logger(500, err);
+      process.exit(-1);
+    }
+    logger(200, data);
+  }
+);
+
+peer.request(REQUESTS.GET_ORDERS, { timeout: 10000 }, async (err, data) => {
   if (err) {
     logger(500, err);
-    process.exit(-1)
-  }
-  logger(200, data);
-})
-
-peer.request(REQUESTS.GET_ORDERS, { timeout: 10000 }, async(err, data) => {
-  if (err) {
-    logger(500, err)
-    process.exit(-1)
+    process.exit(-1);
   }
   logger(200, data);
 
   /** Sync with the own order book */
-  
+
   OrderService.syncOrder(data);
-  /** 
-   * Create Buy Order 
+  /**
+   * Create Buy Order
    * currencyType, exchangeRate, amount, peer, WalletService, OrderService, id
    * */
-  const result = await Promise.all[
-    createBuyOrder(1, 221, 1, peer ,WalletService, OrderService, clientName),
+  const buyOrders = await Promise.all[
+    (createBuyOrder(1, 221, 1, peer, WalletService, OrderService, clientName),
     createBuyOrder(1, 220, 2, peer, WalletService, OrderService, clientName),
     createBuyOrder(2, 1, 120, peer, WalletService, OrderService, clientName),
     createBuyOrder(2, 1, 300, peer, WalletService, OrderService, clientName),
     createBuyOrder(2, 0.9, 223, peer, WalletService, OrderService, clientName),
-    createBuyOrder(2, 1.1, 120, peer, WalletService, OrderService, clientName)]
-    console.log('00000>', result)
-  logger(20, [result]); 
-  /** Create Sell Order */
-  // createBuyOrder(peer, WalletService, OrderService, clientName);
+    createBuyOrder(2, 1.1, 120, peer, WalletService, OrderService, clientName))
+  ];
 
+  /** Create Sell Order */
+  const sellOrders = await Promise.all[
+    (createSellOrder(2, 1, 1000, peer, WalletService, OrderService, clientName),
+    createBuyOrder(2, 1, 20000, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 1, 120, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 1, 300, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 0.9, 223, peer, WalletService, OrderService, clientName),
+    createBuyOrder(1, 1.1, 120, peer, WalletService, OrderService, clientName))
+  ];
+
+  logger(20, [buyOrders]);
+
+  logger(20, [sellOrders]);
+  // createBuyOrder(peer, WalletService, OrderService, clientName);
 });
