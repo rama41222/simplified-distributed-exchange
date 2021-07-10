126542ffae48c10667e19179a4f9fadf6d83fe62
rama41222 <dinushankanrg@gmail.com>
2021-07-10 20:19:34 +0530

    feat: project init

---
+++ server/src/modules/orders/order.service.js
@@ -0,0 +1,34 @@
+const OrderType = require('./type.model');
+
+module.exports = () => {
+    
+    let orders = new Array();
+    
+    removeOrder = (id) => {
+        orders.filter(order => (order.id !== id));
+    }
+    
+    getOrders = () => {
+        return orders;
+    }
+    
+    getOrderById = (id) => {
+        return orders.find(order => (order.id === id));
+    }
+    
+    getOrdersByType = (type) => {
+        return orders.find(order => (order.type === OrderType.types[type]));
+    }
+
+    publishOrder = (order) => {
+        orders.push(order)
+    }
+        
+    return {
+        publishOrder,
+        getOrders,
+        removeOrder,
+        getOrderById,
+        getOrdersByType
+    }
+}
